9e0aad0eb2787721d126061e1d51a8c437fa8973
xewoks <76702828+xewoks@users.noreply.github.com>
2021-02-26 15:22:53 -0500

    v1.3 added tutorial

--- index.bat
+++ index.bat
@@ -8,12 +8,14 @@ echo 1) Build
 echo 2) Run
 echo 3) init
 echo 4) clean
+echo 5) Tutorial
 echo -
 set /p op=Type option:
 if "%op%"=="1" goto Build
 if "%op%"=="2" goto Run
 if "%op%"=="3" goto init
 if "%op%"=="4" goto clean
+if "%op%"=="5" goto tut
 
 :Build
 cls
@@ -22,6 +24,8 @@ cd %mypath%
 cargo build
 pause
 goto :begin
+
+
 :Run
 cls
 SET mypath=%~dp0
@@ -46,6 +50,19 @@ cls
 SET mypath=%~dp0
 cd %mypath%
 cargo clean
+
+goto :begin
+
+:tut
+cls
+
+echo Welcome to the Tutorial!
+echo.
+echo To use this program all you need are these few steps.
+echo.
+echo First thing you need to have is rust installed
+echo.
+echo Last step is to have c++ build tools installed https://visualstudio.microsoft.com/downloads/
 
 goto :begin
 
