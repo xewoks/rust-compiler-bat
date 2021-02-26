db72be64e609c601abb192c8a2e1c4105922355b
xewoks <76702828+xewoks@users.noreply.github.com>
2021-02-26 14:08:22 -0500

    added cargo clean and some minor changes

--- index.bat
+++ index.bat
@@ -1,4 +1,5 @@
-echo off
+@echo off
+cls
 :begin
 echo Select a task:
 echo =============
@@ -6,33 +7,46 @@ echo -
 echo 1) Build
 echo 2) Run
 echo 3) init
+echo 4) clean
 echo -
 set /p op=Type option:
 if "%op%"=="1" goto Build
 if "%op%"=="2" goto Run
 if "%op%"=="3" goto init
+if "%op%"=="4" goto clean
 
 :Build
-
+cls
 SET mypath=%~dp0
 cd %mypath%
 cargo build
 pause
-
+goto :begin
 :Run
-
+cls
 SET mypath=%~dp0
 cd %mypath%
 cargo run
+goto :begin
 pause
 
 
 :init
 cls
 
-echo running cargo init...
-
 SET mypath=%~dp0
 cd %mypath%
 cargo init
+
+goto :begin
+
+:clean
+cls
+
+SET mypath=%~dp0
+cd %mypath%
+cargo clean
+
+goto :begin
+
 pause
